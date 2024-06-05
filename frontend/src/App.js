e1364b2f860f0dedbb77290b2934c4b7af0b8438
manju.mani <manju.mani@beo.in>
2024-06-05 18:14:42 +0530

    calcu latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,28 @@ import "./App.css";
 
 
 export default function App() {
+  //button values for creating button 
   const numbers = ['+','-','*','/','=','1','2','3','4','5','6','7','8','9','0','C']
+  //declared a useState hook for  showing the values
   const [num,setNum] =useState([])
+  //master function 
   function processCalcu(elem){
     setNum(prevNum => [...prevNum, elem]);
+    if(elem === "="){
+      const exp = num.join('');
+      const result = eval(exp);
+      console.log(result);
+      setNum([result.toString()])
+    }
+    if(elem ==="C"){
+      setNum([])
+    }
+
   }
   return (
     <div>
       <div className="Container">
-        <div className='Display'>{num.join(' ')}
+        <div className='Display'>{num.join(' ')}  
         </div>
         <div>
           {numbers.map((element) => (
