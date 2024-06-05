3ce60193cd249a7fc40e461c658496bece12d298
manju.mani <manju.mani@beo.in>
2024-06-05 17:34:12 +0530

    calculator codes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React,{ useState } from 'react'
+import "./App.css";
 
-function App() {
+
+export default function App() {
+  const numbers = ['+','-','*','/','=','1','2','3','4','5','6','7','8','9','0','C']
+  const [num,setNum] =useState([])
+  function processCalcu(elem){
+    setNum(prevNum => [...prevNum, elem]);
+  }
   return (
-    <div className="App">
-     <p>{{a:1, b:2}}</p>
+    <div>
+      <div className="Container">
+        <div className='Display'>{num.join(' ')}
+        </div>
+        <div>
+          {numbers.map((element) => (
+            <button key={element} onClick={()=>processCalcu(element)}>{element}</button>
+          ))};
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
